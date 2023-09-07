31d74afc4d54bb8cf21d7c621df99849f6fc8843
Şirin Ekşi <sirin.eksi@hotmail.com>
2023-09-07 14:23:53 +0300

    Initial Commit

---
+++ TemelBilesenler/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TemelBilesenler
+//
+//  Created by ceksi on 7.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TemelBilesenler/TemelBilesenlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemelBilesenlerApp.swift
+//  TemelBilesenler
+//
+//  Created by ceksi on 7.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct TemelBilesenlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
